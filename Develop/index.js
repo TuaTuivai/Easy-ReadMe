d84007e4ec44fb8861cf4a1e3323683b4652cf63
TuaTuivai <TuaMTuivai@gmail.com>
2022-12-14 21:43:30 -0800

    finish touches on readME

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,8 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require("fs");
-// const fs       = require('./utils/generateMarkdown')
 const generateMarkdown = require("./utils/generateMarkdown")
-// TODO: Create an array of questions for user input
 inquirer
     .prompt([
-        // WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
         {
             type:'input',
             message:'what was the title of this project?',
@@ -49,9 +45,7 @@ inquirer
     ])
     .then((answers) => {
         var generatedReadme = generateMarkdown(answers)
-        // console.log(generatedReadme);
-
-
+       
         fs.writeFile("Sample.md", generatedReadme, function() {
             console.log("Readme successfully created!")
         })
@@ -60,11 +54,11 @@ inquirer
 
 
 
-// TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
+
 function init() {}
 
-// Function call to initialize app
+
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,5 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if(license == "MIT") {
     return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
@@ -11,30 +9,25 @@ function renderLicenseBadge(license) {
     return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
   }
 }
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
   if(license == "MIT") {
-    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    return "[![License: MIT](https://opensource.org/licenses/MIT)"
   }
   if(license == "Apache") {
-    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    return "[![License](https://opensource.org/licenses/Apache-2.0)"
   }
   if(license == "IBM") {
-    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    return "[![License: IPL 1.0](https://opensource.org/licenses/IPL-1.0)"
   }
 }
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
-  return `Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.`
+  if(license == 'MIT')
+  return `Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so.`
+  if(license == 'Apache')
+  return `THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
+  if(license == 'IBM')
+  return `"Contributor" means IBM and any other entity that distributes the Program.`
 }
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(answers) {
   return `
   ${renderLicenseBadge(answers.license)}
@@ -47,16 +40,16 @@ function generateMarkdown(answers) {
   ${answers.usage}
   
   ## License
-  Link: ${renderLicenseLink(answers.license)}
+  ${renderLicenseLink(answers.license)}
 
   ## GitHub
-  Link: ${answers.github}
+  ${answers.github}
 
   ## Email Address
-  Link: ${answers.email}
+  ${answers.email}
 
   ## Section
-  Link: ${renderLicenseSection(answers.renderLicenseSection)}
+  Link: ${renderLicenseSection(answers.license)}
   `
 }
 
